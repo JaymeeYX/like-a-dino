0f8f843de997d58498292b4531cfcf613149cab6
JaymeeYX <87964859+JaymeeYX@users.noreply.github.com>
2021-07-26 23:26:13 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,277 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        # # # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        # # # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . # .
+        # # # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        `)
+})
+basic.forever(function () {
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.playTone(784, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(587, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.stopAllSounds()
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Double))
+    music.stopAllSounds()
+})
